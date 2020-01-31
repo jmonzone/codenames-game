99354bdbf2472c19f712a357424fd5ffac03b5b7
Jonan <jjsalomonzon@gmail.com>
2020-01-31 11:41:03 -0500

    removed socket.io

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,10 @@
-let socket = io();
-
-socket.on('connect', () => {
-  var startButton = document.getElementById('start');
-  startButton.addEventListener('click', () => {
-    socket.emit('startRequest');
-  });
-});
-
-socket.on('disconnect', function() {
-  console.log('Disconnected from server.');
-});
-
-socket.on('messageSent', (message) => {
+function announce(message) {
   var chatText = document.getElementById('messages');
   chatText.innerHTML += '<div>' + message + '</div>';
-});
+}
 
-socket.on('gameStarted', () => {
-  socket.emit('hintRequest');
-});
+function start(){
+  var startButton = document.getElementById('start');
+  startButton.remove();
+  announce('Game Started.');
+}
