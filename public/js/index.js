ccc62b9f4e02b7c12ea7752c81df918e6725c37e
Jonan <jjsalomonzon@gmail.com>
2020-02-04 16:34:00 -0500

    added word and hint display

--- public/js/index.js
+++ public/js/index.js
@@ -7,4 +7,16 @@ function start(){
   var startButton = document.getElementById('start');
   startButton.remove();
   announce('Game Started.');
+  announce('Hint: ' + displayHint());
+  announce('Words: ' + displayWords());
+}
+
+function displayHint(){
+  var hint = "[CODENAME] [COUNT]";
+  return hint;
+}
+
+function displayWords(){
+  var words = ['[WORD]','[WORD]','[WORD]','[WORD]','[WORD]'];
+  return words;
 }
