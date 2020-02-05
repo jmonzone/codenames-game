b499d8ee58c95244861352b322969067c839470e
Jonan <jjsalomonzon@gmail.com>
2020-02-05 15:54:08 -0500

    added word display and write words.json

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+const fs = require('fs');
 
 const publicPath = path.join(__dirname,'./public');
 const port = process.env.PORT || 3000;
 
 let app = express();
 let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 server.listen(port, () => {
   console.log('Server is up on port ' + port);
+});
+
+io.on('connect', (socket) => {
+
+  socket.on('wordsCreated', (words) => {
+    var dictstring = JSON.stringify(words);
+    fs.writeFile("words.json", dictstring, (err, result) => {
+      if(err)
+        console.log('error', err);
+    });
+    
+    console.log(dictstring);
+  });
+
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+let socket = io();
+
 function announce(message) {
   var chatText = document.getElementById('messages');
   chatText.innerHTML += '<div>' + message + '</div>';
@@ -6,17 +8,82 @@ function announce(message) {
 function start(){
   var startButton = document.getElementById('start');
   startButton.remove();
-  announce('Game Started.');
-  announce('Hint: ' + displayHint());
-  announce('Words: ' + displayWords());
+
+  var words = createWords();
+  displayWords(words);
+
+  socket.emit('wordsCreated', words);
+
+  var hint = createHint();
+  displayHint(hint);
 }
 
-function displayHint(){
-  var hint = "[CODENAME] [COUNT]";
-  return hint;
+function displayWords(words){
+  //HTML reference to DOM element to display the words
+  var display = document.getElementById('word-display');
+
+  words.forEach((word) => {
+    var cell = document.createElement('div');
+    cell.className = 'word';
+    cell.innerHTML = word.string;
+
+    switch (word.color) {
+      case 'blue':
+        cell.style.backgroundColor = 'lightblue';
+        break;
+      case 'red':
+        cell.style.backgroundColor = 'darkorange';
+        break;
+      case 'black':
+        cell.style.backgroundColor = 'darkgrey';
+        break;
+    }
+
+    display.append(cell);
+  });
 }
 
-function displayWords(){
-  var words = ['[WORD]','[WORD]','[WORD]','[WORD]','[WORD]'];
+function createWords(){
+  //Array to store the actual words to display as strings
+  var wordStrings = createWordStrings();
+
+  //Array to store a color map that determines which words are good, bad, or the assassin
+  var colorMap = createColorMap();
+
+  //Array to store objects containing words and their corresponding colors
+  var words = [];
+
+  //Creating the words array using the wordStrings and colorMap
+  for(var i = 0; i < colorMap.length; i++){
+    var word = {
+      string: wordStrings[i],
+      color: colorMap[i]
+    };
+    words.push(word);
+  }
+
   return words;
+}
+
+//TEMP
+function createWordStrings(){
+  var word = ['[WORD1]','[WORD2]','[WORD3]','[WORD4]','[WORD5]','[WORD6]','[WORD7]','[WORD8]','[WORD9]'];
+  return word;
+}
+
+//TEMP
+function createColorMap(){
+  var map = ['blue','red','blue','black','blue','blue','red','blue','blue'];
+  return map;
+}
+
+//TEMP
+function displayHint(hint){
+  announce('Hint: ' + hint);
+}
+
+//TEMP
+function createHint(){
+  var hint = '[HINT]'
+  return hint;
 }
