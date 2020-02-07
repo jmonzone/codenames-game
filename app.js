76a97d184f2965ee3f3f86dec0e410a00797cd94
Jonan <jjsalomonzon@gmail.com>
2020-02-07 05:22:10 -0500

    updated frontend interface

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ server.listen(port, () => {
 
 io.on('connect', (socket) => {
 
+  var cachedHint = ""
   socket.on('wordsCreated', (words) => {
 
     var jsonWords = JSON.stringify(words);
@@ -27,6 +28,8 @@ io.on('connect', (socket) => {
     var hint = createHint(jsonWords, (hint) => {
       io.to(socket.id).emit('clearMessages');
       io.to(socket.id).emit('hintGiven', hint);
+      io.to(socket.id).emit('messageSent', 'Select 3 different words.');
+      cachedHint = hint;
     });
 
   });
@@ -37,11 +40,55 @@ io.on('connect', (socket) => {
     if (!cachedWords.includes(word.string)){
       selectedWords.push(word);
       cachedWords.push(word.string);
+
+      io.to(socket.id).emit('clearMessages');
+      io.to(socket.id).emit('messageSent', 'Hint: ' + cachedHint.toUpperCase());
+      // io.to(socket.id).emit('messageSent', 'Select ' + (3 - selectedWords.length) + ' more words.');
+
+      var selectsLeft = 3 - selectedWords.length;
+      if (selectsLeft > 0)
+        io.to(socket.id).emit('messageSent', 'Select ' + selectsLeft + ' more words.');
     }
 
     if (selectedWords.length == 3){
-      var jsonResults = JSON.stringify(results);
-      io.to(socket.id).emit('messageSent', 'JSON results: ' +  jsonResults);
+      var score = 0;
+      var goodWords = [];
+      var badWords = [];
+
+      selectedWords.forEach((word) => {
+        switch (word.color) {
+          case 'blue':
+            score++;
+            goodWords.push(word.string);
+            break;
+          case 'red':
+            score--;
+            badWords.push(word.string);
+            break;
+          case 'black':
+            score -= 2;
+            badWords.push(word.string + ' (ASSASSIN)');
+            break;
+        }
+      });
+
+      var goodWordsStr = "";
+
+      goodWords.forEach((word) => {
+        goodWordsStr += ' ' + word.toUpperCase() + ',';
+      });
+      goodWordsStr = goodWordsStr.substring(0, goodWordsStr.length - 1);
+
+      var badWordsStr = "";
+
+      badWords.forEach((word) => {
+        badWordsStr += ' ' + word.toUpperCase() + ',';
+      });
+      badWordsStr = badWordsStr.substring(0, badWordsStr.length - 1);
+
+      io.to(socket.id).emit('messageSent', 'Score: ' + score);
+      io.to(socket.id).emit('messageSent', 'Correct Answers: ' + goodWordsStr);
+      io.to(socket.id).emit('messageSent', 'Wrong Answers: ' + badWordsStr);
     }
   })
 
--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ socket.on('messageSent', (message) => {
 
 socket.on('clearMessages', () => {
   var announcements = document.getElementById('messages');
-  announcements.innerHTML = ""
+  announcements.innerHTML = "";
 });
 
 function announce(message) {
@@ -22,10 +22,20 @@ function start(){
   var startButton = document.getElementById('start');
   startButton.remove();
 
-  var words = createWords();
-  displayWords(words);
+  var announcements = document.getElementById('messages');
+  announcements.innerHTML = "";
 
+  var display = document.createElement('div');
+  display.id = 'word-display';
+
+  var display_wrapper = document.getElementById('word-display-wrapper');
+  display_wrapper.append(display);
+
+  var words = createWords();
   socket.emit('wordsCreated', words);
+
+  var wordDisplays = displayWords(words);
+  createColorChanger(wordDisplays);
 }
 
 socket.on('hintGiven', (hint) => {
@@ -33,32 +43,36 @@ socket.on('hintGiven', (hint) => {
 });
 
 function displayWords(words){
+
+  var retVal = []
   //HTML reference to DOM element to display the words
   var display = document.getElementById('word-display');
 
   words.forEach((word) => {
     var cell = document.createElement('BUTTON');
     cell.className = 'word';
-    cell.innerHTML = word.string;
+    cell.id = 'word_' + word.string;
+    cell.innerHTML = word.string.toUpperCase();
+    cell.style.backgroundColor = 'white';
 
-    switch (word.color) {
-      case 'blue':
-        cell.style.backgroundColor = 'lightblue';
-        break;
-      case 'red':
-        cell.style.backgroundColor = 'darkorange';
-        break;
-      case 'black':
-        cell.style.backgroundColor = 'darkgrey';
-        break;
-    }
-
-    cell.addEventListener('click', () => {
-      socket.emit('wordSelected', word);
+    socket.on('hintGiven', (hint) => {
+      cell.addEventListener('click', () => {
+        socket.emit('wordSelected', word);
+      });
     });
+
+
+    var cellData = {
+      id: cell.id,
+      color: word.color,
+    };
+
+    retVal.push(cellData);
 
     display.append(cell);
   });
+
+  return retVal;
 }
 
 function createWords(){
@@ -86,17 +100,75 @@ function createWords(){
 //TEMP
 function createWordStrings(){
   // var words = ['[WORD1]','[WORD2]','[WORD3]','[WORD4]','[WORD5]','[WORD6]','[WORD7]','[WORD8]','[WORD9]'];
-  var words = ['apple','computer','japan','glasses','bag','fish','italy','dictionary','book'];
+  var wordPool =
+    ['apple','computer','japan','glasses','bag','fish','italy','dictionary','book', 'leather',
+    'husband','breakfast','lady','silk','festival','spirit','medicine','bike','plastic','stone'];
+  var words = [];
+  var numWords = 8;
+
+  for(var i = 0; i < numWords; i++){
+    let j = Math.floor(Math.random() * wordPool.length);
+    words.push(wordPool[j]);
+    wordPool.splice(j,1);
+  }
+
   return words;
 }
 
 //TEMP
 function createColorMap(){
-  var map = ['blue','red','blue','black','blue','blue','red','red'];
-  return map;
+
+  var colorPool = ['blue','red','blue','black','blue','blue','red','red'];
+  var colors = [];
+  var numWords = 8;
+
+  for(var i = 0; i < numWords; i++){
+    let j = Math.floor(Math.random() * colorPool.length);
+    colors.push(colorPool[j]);
+    colorPool.splice(j,1);
+  }
+
+  return colors;
 }
 
 //TEMP
 function displayHint(hint){
-  announce('Hint: ' + hint);
+  announce('Hint: ' + hint.toUpperCase());
+}
+
+function createColorChanger(wordDisplays){
+
+  var color_changer = document.createElement('button');
+  color_changer.innerHTML = "Reveal Colors";
+
+  var interactions = document.getElementById('interactions');
+  interactions.append(color_changer)
+
+  color_changer.addEventListener('click', () => {
+    wordDisplays.forEach((word) => {
+      var cell = document.getElementById(word.id);
+
+      if (cell.style.backgroundColor == 'white') {
+
+        color_changer.innerHTML = "Hide Colors";
+        switch (word.color) {
+          case 'blue':
+            cell.style.backgroundColor = 'lightblue';
+            break;
+          case 'red':
+            cell.style.backgroundColor = 'darkorange';
+            break;
+          case 'black':
+            cell.style.backgroundColor = 'darkgrey';
+            break;
+        }
+
+      } else {
+
+        color_changer.innerHTML = "Reveal Colors";
+        cell.style.backgroundColor = 'white'
+
+      }
+    });
+  });
 }
