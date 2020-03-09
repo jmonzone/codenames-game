c1e0270f6f6a4846508539be92fe1568f965526a
Jonan <jjsalomonzon@gmail.com>
2020-03-09 02:25:04 -0400

    heroku error

--- python/test-scripts/get-words.py
+++ python/test-scripts/get-words.py
@@ -7,20 +7,20 @@ import random
 
 f = open("/Users/johnnanmonzon/Documents/web-projects/codenames/python/word-embeddings/glove-embeddings.txt");
 
-def isValidWord(word):
-    if word.isalpha():
-        return True
-    return False
-
-numWords = 8
-
-words = []
-for line in f:
-    values = line.split()
-    word = values[0]
-    if isValidWord(word):
-        words.append(values[0])
-
-
-words = random.choices(words, k=numWords)
-print(words)
+# def isValidWord(word):
+#     if word.isalpha():
+#         return True
+#     return False
+#
+# numWords = 8
+#
+# words = []
+# for line in f:
+#     values = line.split()
+#     word = values[0]
+#     if isValidWord(word):
+#         words.append(values[0])
+#
+#
+# words = random.choices(words, k=numWords)
+# print(words)
